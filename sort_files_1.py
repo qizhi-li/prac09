5a9ce653e74c458516b146120ee50aca865f4314
qizhli <qizhi.li@my.jcu.edu.au>
2022-05-23 23:13:23 +0800

    y

---
+++ sort_files_1.py
@@ -0,0 +1,26 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+
+"""
+import os
+
+
+def main():
+
+    os.chdir("FilesToSort")
+    for filename in os.listdir('.'):
+
+        if os.path.isdir(filename):
+            continue
+
+        extension = filename.split('.')[-1]
+
+        try:
+            os.mkdir(extension)
+        except FileExistsError:
+            pass
+        print("{}/{}".format(extension, filename))
+        os.rename(filename, "{}/{}".format(extension, filename))
+
+
+main()
