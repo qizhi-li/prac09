a735c0cf8abc22c9302a3abd9a29985f05c50a7c
qizhli <qizhi.li@my.jcu.edu.au>
2022-05-23 23:16:27 +0800

    y

---
+++ sort_files_2.py
@@ -0,0 +1,33 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Sort files based on extension and user categorisation
+"""
+import os
+
+
+def main():
+
+    # The following dictionary will allow us to map extensions to the destination folder names
+    extension_to_category = {}
+    os.chdir("FilesToSort")
+    for filename in os.listdir('.'):
+
+        if os.path.isdir(filename):
+            continue
+
+        extension = filename.split('.')[-1]
+        if extension not in extension_to_category:
+            category = input("What category would you like to sort {} files into? ".format(extension))
+
+            extension_to_category[extension] = category
+            try:
+
+                os.mkdir(category)
+            except FileExistsError:
+                pass
+
+
+        os.rename(filename, "{}/{}".format(extension_to_category[extension], filename))
+
+
+main()
