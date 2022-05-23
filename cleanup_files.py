895d1cd1af2ee8ebc00fe898c103ff433f7f5084
qizhli <qizhi.li@my.jcu.edu.au>
2022-05-23 23:12:47 +0800

    y

---
+++ cleanup_files.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+
+"""
+import os
+
+
+def main():
+    """Cleanup inconsistent song lyrics file names."""
+
+    # Change to desired directory
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+
+
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, new_name)
+            os.rename(full_name, new_name)
+
+
+def get_fixed_filename(filename):
+    """Return a 'fixed' version of filename."""
+
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+    return new_name
+
+
+main()
